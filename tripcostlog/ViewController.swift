8daadde648cc36044fa7bc9bbefbb7629ad34c4b
takari.com <takari.com>
2020-11-25 21:51:38 +0900

    Initial Commit

---
+++ tripcostlog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tripcostlog
+//
+//  Created by 高木龍之介 on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
